5728f2f1b86c800a966b6f69baacbaaf5c021d19
Lapide <64395933+nonumbershere@users.noreply.github.com>
2021-06-10 14:50:17 -0400

    Create ExecuteCommand.js

---
+++ examples/ExecuteCommand.js
@@ -0,0 +1,43 @@
+var RunCommand = {
+   commands: [],
+   prefix: "",
+   run_command: function(command) {
+      for (var i = 0; i < this.commands.length; ++i) {
+	  if (this.commands[i] == null) {
+	  
+	  } else {
+	      if (command == this.prefix+this.commands[i].command) {
+		     this.commands[i].do();
+		  }
+		}
+	  }
+   },
+   create_command: function(name, func) {
+     this.commands.push({command: name, do: func});
+   },
+   remove_command: function(name) {
+      for (var i = 0; i < this.commands.length; ++i) {
+	     if (name == this.commands[i].command) {
+		 this.commands[i] = null;
+		 }
+	  }
+   },
+   run_commandSes: function() {
+      for (var i = 0; i < this.commands.length; ++i) {
+	  if (this.commands[i] == null) {
+	  
+	  } else {
+	      if (command == this.prefix+this.commands[i].command) {
+		     this.commands[i].do();
+		  }
+		}
+	  }
+
+     
+   }
+}
+
+RunCommand.create_command("hello", function() {
+   console.log("Hello World!");
+});
+RunCommand.run_command("hello");
