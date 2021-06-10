0190799b258867cdfdc22344c80a2f031e9823b1
Lapide <64395933+nonumbershere@users.noreply.github.com>
2021-06-10 14:48:48 -0400

    Create RunCommand.js

---
+++ RunCommand.js
@@ -0,0 +1,38 @@
+var RunCommand = {
+   commands: [],
+   prefix: "",
+   run_command: function(command) {
+      for (var i = 0; i < this.commands.length; ++i) {
+	  if (this.commands[i] == null) {
+	  
+	  } else {
+	      if (command == this.prefix+this.commands[i].command) {
+		     this.commands[i].do();
+		  }
+		}
+	  }
+   },
+   create_command: function(name, func) {
+     this.commands.push({command: name, do: func});
+   },
+   remove_command: function(name) {
+      for (var i = 0; i < this.commands.length; ++i) {
+	     if (name == this.commands[i].command) {
+		 this.commands[i] = null;
+		 }
+	  }
+   },
+   run_commandSes: function() {
+      for (var i = 0; i < this.commands.length; ++i) {
+	  if (this.commands[i] == null) {
+	  
+	  } else {
+	      if (command == this.prefix+this.commands[i].command) {
+		     this.commands[i].do();
+		  }
+		}
+	  }
+
+     
+   }
+}
